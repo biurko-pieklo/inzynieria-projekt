2862ca3814e1a44b1aae94e055c8c92658170d8f
Matthew Cherry <matiwisnia16@gmail.com>
2022-12-19 20:43:05 +0100

    Display files from upload folder

--- index.php
+++ index.php
@@ -6,5 +6,6 @@ spl_autoload_register(function($class_name) {
 
 require_once('./theme/parts/header.php');
 require_once('./theme/parts/upload.php');
+require_once('./theme/parts/filelist.php');
 require_once('./theme/parts/footer.php');
 ?>
